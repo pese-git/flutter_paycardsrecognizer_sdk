fbd25f4ff170171443be9d9d2bca9ef15be54679
Alexey Popkov <alexey.popkov@technorely.com>
2021-12-24 16:45:27 +0200

    refactoring

--- ios/Classes/SwiftFlutterPaycardsrecognizerSdkPlugin.swift
+++ ios/Classes/SwiftFlutterPaycardsrecognizerSdkPlugin.swift
@@ -2,127 +2,43 @@ import Flutter
 import UIKit
 import PayCardsRecognizer
 
-//public class SwiftFlutterPaycardsrecognizerSdkPlugin: NSObject, FlutterPlugin, PayCardsRecognizerPlatformDelegate {
-//  var recognizer: PayCardsRecognizer?
-//  var _result: FlutterResult?
-//
-//  var _viewController: UIViewController
-//
-//  lazy var backButton = createBackButton()
-//
-//    public override init() {
-//    }
-////  init(viewController: UIViewController) {
-////      self._viewController = viewController
-////  }
-//
-//  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-//    //result("iOS " + UIDevice.current.systemVersion)
-//    guard call.method == "scanCard" else {
-//        result(FlutterMethodNotImplemented)
-//        return
-//    }
-//
-//    if (self._result != nil) {
-//        result(FlutterError(code: "ALREADY_ACTIVE", message: "Scan card is already active", details: nil))
-//        return
-//    }
-//    self._result = result
-//    self.recognizer = PayCardsRecognizer(delegate: self, resultMode: .async, container: self._viewController.view, frameColor: .green)
-//    NSLog("Strart recognized card")
-//
-//    self.recognizer?.startCamera()
-//
-//
-//    _viewController.view.addSubview(backButton)
-//    _viewController.view.bringSubviewToFront(backButton)
-//  }
-//
-//    @objc func goBack(){
-//        self.recognizer?.stopCamera()
-//        backButton.removeFromSuperview()
-//        self._result = nil
-//    }
-//
-//  // PayCardsRecognizerPlatformDelegate
-//    public func payCardsRecognizer(_ payCardsRecognizer: PayCardsRecognizer, didRecognize recognizeResult: PayCardsRecognizerResult) {
-//    NSLog("Parse card data")
-//      let cardDict: [String: Any?] = ["cardHolderName": recognizeResult.recognizedHolderName,
-//                            "cardNumber": recognizeResult.recognizedNumber,
-//                            "expiryMonth": recognizeResult.recognizedExpireDateMonth,
-//                            "expiryYear": recognizeResult.recognizedExpireDateYear]
-//
-//
-//    NSLog("Send card data")
-//    self._result?(cardDict)
-//
-//    NSLog("Finish recognized card")
-//      self.recognizer?.stopCamera()
-//      self._result = nil
-//  }
-//}
+fileprivate let flutterChannelName = "flutter_paycardsrecognizer_sdk"
+fileprivate let flutterMethodName = "scanCard"
 
-// MARK: - ui components
-//extension SwiftFlutterPaycardsrecognizerSdkPlugin {
-//    private func createBackButton() -> UIButton {
-//        let result = UIButton(frame: CGRect.zero)
-//        result.backgroundColor = .clear
-//        result.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
-//        result.setTitle("✕", for: .normal)
-//        result.addTarget(self, action: #selector(goBack), for: .touchUpInside)
-//        return result
-//    }
-//}
-//
-//// MARK: - factory
-//extension SwiftFlutterPaycardsrecognizerSdkPlugin {
-//    public static func register(with registrar: FlutterPluginRegistrar) {
-//        let channel = FlutterMethodChannel(name: "flutter_paycardsrecognizer_sdk", binaryMessenger: registrar.messenger())
-//        let rootViewController: UIViewController = (UIApplication.shared.delegate?.window??.rootViewController)!
-//
-//        let plugin = SwiftFlutterPaycardsrecognizerSdkPlugin()
-//        var vc = RecognizerViewController(nibName: nil, bundle: nil);
-//
-//
-////        let instance = SwiftFlutterPaycardsrecognizerSdkPlugin(viewController: viewController)
-//
-//        //        viewController.present(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
-//
-//        //        viewController.dismiss(animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
-//
-//        registrar.addMethodCallDelegate(instance, channel: channel)
-//    }
-//}
-
-//typealias SwiftFlutterPaycardsrecognizerSdkPlugin =
-// MARK: -
-
-public class SwiftFlutterPaycardsrecognizerSdkPlugin: NSObject, FlutterPlugin, RecognizerVCDelegate {
+public class SwiftFlutterPaycardsrecognizerSdkPlugin:
+    NSObject,
+    FlutterPlugin,
+    RecognizerVCDelegate {
     
     var vc: UIViewController?
     
     var _flutterResultHandler: FlutterResult?
     
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "flutter_paycardsrecognizer_sdk", binaryMessenger: registrar.messenger())
+        let channel = FlutterMethodChannel(name: flutterChannelName,
+                                           binaryMessenger: registrar.messenger())
         let plugin = SwiftFlutterPaycardsrecognizerSdkPlugin()
         registrar.addMethodCallDelegate(plugin, channel: channel)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        guard call.method == "scanCard" else {
+        guard call.method == flutterMethodName else {
             result(FlutterMethodNotImplemented)
             return
         }
         
         if (self._flutterResultHandler != nil) {
-            result(FlutterError(code: "ALREADY_ACTIVE", message: "Scan card is already active", details: nil))
+            result(FlutterError(code: "ALREADY_ACTIVE",
+                                message: "Scan card is already active",
+                                details: nil))
             return
         }
         
         self._flutterResultHandler = result
         
-        showRecognizerVC()
+        DispatchQueue.main.async {
+            self.showRecognizerVC()
+        }
     }
     
     func showRecognizerVC() {
@@ -138,31 +54,43 @@ public class SwiftFlutterPaycardsrecognizerSdkPlugin: NSObject, FlutterPlugin, R
     func recognizerVC(_ sender: RecognizerVC, didRecognize result: [String : Any?]) {
         _flutterResultHandler?(result)
         _flutterResultHandler = nil
-        vc?.dismiss(animated: true, completion: nil)
+        DispatchQueue.main.async {
+            self.vc?.dismiss(animated: true, completion: nil)
+        }
     }
     
     func dismissRecognizerVC(_ sender: RecognizerVC) {
         _flutterResultHandler = nil
-        vc?.dismiss(animated: true, completion: nil)
+        DispatchQueue.main.async {
+            self.vc?.dismiss(animated: true, completion: nil)
+        }
     }
 }
 
+// MARK: - RecognizerVCDelegate
 protocol RecognizerVCDelegate: AnyObject {
     func recognizerVC(_ sender: RecognizerVC, didRecognize result: [String: Any?])
     func dismissRecognizerVC(_ sender: RecognizerVC)
 }
 
+// MARK: - RecognizerVC
 class RecognizerVC: UIViewController, PayCardsRecognizerPlatformDelegate {
     
-    lazy var recognizer = PayCardsRecognizer(delegate: self, resultMode: .sync, container: self.view, frameColor: .green)
+    lazy var recognizer = PayCardsRecognizer(delegate: self,
+                                             // `async` needed
+                                             // https://github.com/faceterteam/PayCards_iOS/issues/23
+                                             resultMode: .async,
+                                             container: cameraView,
+                                             frameColor: .green)
     
     lazy var backButton = createBackButton()
+    lazy var cameraView = UIView(frame: CGRect.zero)
     
     weak var delegate: RecognizerVCDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(backButton)
+        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -178,8 +106,16 @@ class RecognizerVC: UIViewController, PayCardsRecognizerPlatformDelegate {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        updateFrames()
+    }
+    private func setupUI() {
+        view.addSubview(cameraView)
+        view.addSubview(backButton)
+    }
+    
+    private func updateFrames() {
+        cameraView.frame = view.bounds
         backButton.frame = CGRect(x: 0, y: 16.0, width: 100.0, height: 100.0)
-        view.bringSubviewToFront(backButton)
     }
     
     @objc func goBack(){
@@ -194,18 +130,15 @@ class RecognizerVC: UIViewController, PayCardsRecognizerPlatformDelegate {
         result.addTarget(self, action: #selector(goBack), for: .touchUpInside)
         return result
     }
-    //}
-    //
-    //// MARK: - PayCardsRecognizerPlatformDelegate
-    //
-    //extension RecognizerVC {
-    
-    func payCardsRecognizer(_ payCardsRecognizer: PayCardsRecognizer, didRecognize result: PayCardsRecognizerResult) {
+}
+
+// MARK: - PayCardsRecognizerPlatformDelegate
+extension RecognizerVC {
+    public func payCardsRecognizer(_ payCardsRecognizer: PayCardsRecognizer, didRecognize result: PayCardsRecognizerResult) {
         let cardInfo: [String: Any?] = ["cardHolderName": result.recognizedHolderName,
                                         "cardNumber": result.recognizedNumber,
                                         "expiryMonth": result.recognizedExpireDateMonth,
                                         "expiryYear": result.recognizedExpireDateYear]
-        
         delegate?.recognizerVC(self, didRecognize: cardInfo)
     }
 }
